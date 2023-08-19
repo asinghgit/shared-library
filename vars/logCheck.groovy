0638e43104cbea4b5ed0c1a75345d983336d6621
Alok Singh <33476609+asinghgit@users.noreply.github.com>
2023-08-19 21:56:39 +0530

    Update logCheck.groovy

--- vars/logCheck.groovy
+++ vars/logCheck.groovy
@@ -3,7 +3,7 @@ import org.apache.commons.lang.StringUtils
 def call(String filter_string, int occurrence)
 {
 	def logs = currentBuild.rawBuild.getLog(1000).join('\n')
-	int count = StringUtils(logs,filter_string);
+	int count = StringUtils.countMatches(logs,filter_string);
 	if ( count >=1 )
 	{
 	currentBuild.result='UNSTABLE'
