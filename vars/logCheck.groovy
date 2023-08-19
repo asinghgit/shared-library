4303c5946eb78d0ef3c4029ea3901364cd51fb7c
Alok Singh <33476609+asinghgit@users.noreply.github.com>
2023-08-19 15:51:34 +0530

    Update logCheck.groovy

--- vars/logCheck.groovy
+++ vars/logCheck.groovy
@@ -4,7 +4,7 @@ import org.apache.commons.lang.StringUtils
 
 def call(String filter_string, int occurrence)
 {
-	def logs = currentBuild.rawBuild.rawBuild(1000).join('\n')
+	def logs = currentBuild.rawBuild.getLog(1000).join('\n')
 	int count = StringUtils(logs,filter_string)
 	if ( count >=1 )
 	{
